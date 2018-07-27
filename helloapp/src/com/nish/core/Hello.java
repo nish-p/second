3294192533b32d6921b1af448d130c57ac1f67a9
owner <owner@DellE5420>
2018-07-27 13:40:22 -0400

    first commit

---
+++ helloapp/src/com/nish/core/Hello.java
@@ -0,0 +1,12 @@
+package com.nish.core;
+
+ class Hello {
+	public static void main(String[] args) {
+		int x=0;
+		if(x>0)
+		System.out.println("cool");
+		System.out.println("nice");
+		
+	}
+
+}
